10d605c2206b9e422b61bd6205cd2d36cf90cfb8
JayCodez <133884358+DataMiner92@users.noreply.github.com>
2025-07-29 16:28:33 +0300

    Add .next/static/chunks/src_app_admin_settings_page_tsx_a993527f._.js 1753795712451

---
+++ .next/static/chunks/src_app_admin_settings_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_82fd84a9._.js",
+  "static/chunks/node_modules_7d2a4f0e._.js"
+],
+    source: "dynamic"
+});
