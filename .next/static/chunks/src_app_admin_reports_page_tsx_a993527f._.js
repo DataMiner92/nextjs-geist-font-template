91bd1f7292cf6cefe26471c235bce27abcd19f63
JayCodez <133884358+DataMiner92@users.noreply.github.com>
2025-07-29 16:28:50 +0300

    Add .next/static/chunks/src_app_admin_reports_page_tsx_a993527f._.js 1753795729655

---
+++ .next/static/chunks/src_app_admin_reports_page_tsx_a993527f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_395de579._.js",
+  "static/chunks/src_c1bbcab5._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_jspdf_dist_jspdf_es_min_c277e70f.js",
+  "static/chunks/node_modules_xlsx_xlsx_mjs_ad755052._.js",
+  "static/chunks/node_modules_560d0d70._.js"
+],
+    source: "dynamic"
+});
